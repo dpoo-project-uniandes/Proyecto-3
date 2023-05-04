fb33d7f350564f18c7b3491da8c989ce35e94901
mafedelahoz <m.delah@uniandes.edu.co>
2023-05-04 17:36:37 -0500

    Carga botones menú principal

--- Entrega 2/hotel_system/src/hotel_system/interfaces/HotelSystemInterface.java
+++ Entrega 2/hotel_system/src/hotel_system/interfaces/HotelSystemInterface.java
@@ -1,19 +1,21 @@
 package hotel_system.interfaces;
 
 import java.awt.Color;
+import java.io.IOException;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 
 public class HotelSystemInterface extends JFrame {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException  {
 		HotelSystemInterface HotelSystemInterface = new HotelSystemInterface();
 	}
 	
 	private Login login;
-	
-	public HotelSystemInterface() {
+	private MenuPrincipal menuP;
+
+	public HotelSystemInterface() throws IOException {
 		this.login = new Login();
 		loginConfigurationFrame();
 		componentsFrame();
@@ -25,7 +27,8 @@ public class HotelSystemInterface extends JFrame {
 		this.add(login);
 		
 		// SETTINGS
-		this.getContentPane().setBackground(Color.BLUE);
+		
+		this.getContentPane().setBackground(Color.WHITE);
 		this.setSize(600, 700);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
